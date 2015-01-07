b57b2fd177058be5e7a6ad9c9c3601451badac9c
Jamieson Warner <jamiesonwarner@gmail.com>
2015-01-07 16:23:30 -0600

    remove keen dependency

--- config/build.config.js
+++ config/build.config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   dist: '.',
 
-  jsFiles: ['lib/keen-js/dist/keen-tracker.js', 'src/js/**/*.js'],
+  jsFiles: ['src/js/**/*.js'],
 
   versionData: {
     version: pkg.version
